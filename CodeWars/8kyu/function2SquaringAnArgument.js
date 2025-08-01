726f2e3dacee2f4395a48d35bd4cb39b2f3e3a58
Chukwuemeka Emeche <j0she1ife31@gmail.com>
2025-08-01 16:49:12 -0700

    return square function

---
+++ CodeWars/8kyu/function2SquaringAnArgument.js
@@ -0,0 +1,10 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+// Answer:
+// To get the number squared, I did num**2
+// Which is the number to 2nd power
+function square(num) {
+  return num**2;
+}
