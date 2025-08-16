d6440f39726e9629c66e931feb9b2832caff5c91
Chukwuemeka Emeche <j0she1ife31@gmail.com>
2025-08-15 23:06:38 -0700

    return uppercase

---
+++ CodeWars/8kyu/makeUpperCase.js
@@ -0,0 +1,14 @@
+/*
+Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  // Code here
+}
+*/
+
+// Answer:
+// The answer was simple, I appended the .toUpperCase() to the end of the
+// string to capitalize it entirely
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
