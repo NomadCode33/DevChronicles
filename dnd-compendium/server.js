6cf41fc480641a12d6e2bd5810ec760276c17241
Chukwuemeka Emeche <j0she1ife31@gmail.com>
2025-09-08 21:28:59 -0700

    make the links and pictures work on the server

--- dnd-compendium/server.js
+++ dnd-compendium/server.js
@@ -5,15 +5,11 @@ const PORT = 8000;
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
-
-app.get('/spells', (request, response) => {
-    response.sendFile(__dirname + '/spells.html')
-})
-
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
